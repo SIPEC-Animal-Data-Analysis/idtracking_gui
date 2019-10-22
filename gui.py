12123f875493b5a486e298c8e6b1f4a373e04057
damaggu <markusm2342@gmail.com>
2019-10-23 01:03:52 +0200

    first OOP skeleton

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
-import numpy as np
-import cv2
-from time import sleep
-from tqdm import tqdm
-import time
-
-from argparse import ArgumentParser
 import os
+import time
+from argparse import ArgumentParser
+from time import sleep
+import cv2
+
+
+import numpy as np
+from skimage.transform import rescale
+from tqdm import tqdm
 
 parser = ArgumentParser()
 
@@ -68,26 +70,6 @@ parser.add_argument('--window_size',
                     default=1024,
                     help='size of the GUI in pixels')
 
-args = parser.parse_args()
-
-names = args.names
-names = names.split(',')
-
-name_indicators = [ 'Key/Name:  '+ str(idx) +  ' / ' +  el for idx, el in enumerate(names) ]
-
-filename = args.filename
-sink = './example_data/frames/' + filename + '_segmented/'
-results_sink = args.results_sink
-num_masks = args.num_masks
-window_size = args.window_size
-
-# create results folder if non-existing
-if not os.path.exists(results_sink):
-  os.makedirs(results_sink)
-
-print('loading masks')
-results = np.load('./example_data/masks/' + filename + '_SegResults.npy', allow_pickle=True)
-
 ## look in results sink for already annotated data
 # try:
 #   annotations = (results_sink + 'IDresults_' + filename + '.npy')
@@ -95,147 +77,280 @@ results = np.load('./example_data/masks/' + filename + '_SegResults.npy', allow_
 # except FileNotFoundError:
 #   continue
 
-class MaskHandler():
-  def __init__(self):
-    pass
+class WindowHandler:
+
+    frames = None
+    current_frame = None
+
+    def __init__(self,
+                 frames,
+                 name_indicators,
+                 filename,
+                 results_sink,
+                 masks,
+                 num_masks,
+                 stepsize,
+                 window_size, ) -> None:
+
+        super().__init__()
+        self.masks = masks
+        self.frames = frames
+        self.name_indicators = name_indicators
+        self.filename = filename
+        self.results_sink = results_sink
+        self.current_mask_focus = 0
+        self.zoom = False
+        self.stepsize = stepsize
+        self.break_status = False
+        self.num_masks = num_masks
+
+        # take random masks
+        self.random_mask_indexes = np.random.randint(0, len(self.masks), 1)
+
+        self.current_mask = 0
+        self.current_frame = self.random_mask_indexes[self.current_mask]
+
+        self.mask_focus = 0
+
+        # start timer for persistent saving
+        self.start_time = time.time()
+
+        # opencv params
+
+        cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+        cv2.resizeWindow('output', window_size, window_size)
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.bottomLeftCornerOfText = (512, 512)
+        self.fontScale = 500
+        self.fontColor = (0, 255, 0)
+        self.lineType = 20
+
+        self.results = {}
+
+    # TODO:awggw
+    def load_frames(self):
+        pass
+
+    def close(self):
+        print('writing data, do not interrupt!')
+        self.save_data()
+        print('done writing data')
+        cv2.destroyAllWindows()
+
+    def save_data(self):
+        np.save(self.results_sink + 'IDresults_' + self.filename + '.npy', self.results)
+
+    def clocked_save(self):
+        if ((time.time() - self.start_time) % 300 < 0.055):
+            self.save_data()
+
+    def display_mask(self,
+                     current_mask,
+                     focus=False):
+        if (self.current_frame == self.random_mask_indexes[self.current_mask]):
+            # indicate mask
+            cv2.rectangle(self.frames[self.current_frame],
+                          (current_mask[1], current_mask[0]),
+                          (current_mask[3], current_mask[2]),
+                          (0, 255, 0), 3)
+            # indicate mask number
+            cv2.putText(self.frames[self.current_frame], 'Mask: ' + str(self.current_frame),
+                        (10, 100), self.font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        else:
+            cv2.rectangle(self.frames[self.current_frame],
+                          (current_mask[1], current_mask[0]),
+                          (current_mask[3], current_mask[2]),
+                          (125, 125, 125), 3)
+        if(self.current_frame in self.results.keys()):
+            pass
 
 
-# limit now to 1000 frames for demo purposes and limited masks
-results = results[:500]
-num_masks = 5
+        # # TODO:
+        # for res in own results:
+        #     display masks
 
-# load a couple of frames
-fnames = []
+    def display_frame(self):
+        if(self.zoom):
+            img = self.frames[self.current_frame]
+            y1, x1, y2, x2 = self.masks[self.current_frame]['rois'][self.current_mask_focus]
+            center_x = float(x2 + x1) / 2.0
+            center_y = float(y2 + y1) / 2.0
 
-print('loading frames')
-for i in tqdm(range(0, len(results))):
-  example_frame = np.load(sink + filename + '_frame_' + str(i) + '.npy')
-  fnames.append(example_frame)
+            # TODO: make relative
+            masked_img = img[int(center_y - 200):int(center_y + 200),
+                         int(center_x - 200):int(center_x + 200)]
 
-# take random masks
-idxs = np.random.randint(0, len(results), num_masks)
+            # TODO: determine value or find best fixed
+            rescaled_img = rescale(masked_img, 2.5, multichannel=True)
+            cv2.imshow("output", rescaled_img)
+        else:
+            curr_img = self.frames[self.current_frame]
+            cv2.putText(curr_img, 'Frame: ' + str(self.current_frame), (10, 200),
+                        self.font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow("output", curr_img)
+        return
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (512, 512)
-fontScale = 500
-fontColor = (0, 255, 0)
-lineType = 20
-cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
-cv2.resizeWindow('output', window_size, window_size)
+    def display_current(self):
+        try:
+            current_masks = self.masks[self.current_frame]['rois']
+            for mask in current_masks:
+                self.display_mask(mask)
+            self.display_mask(self.masks[self.current_frame]['rois'][self.current_mask_focus],
+                              focus=True)
+        except KeyError:
+            pass
 
-res = []
-stepsize = 1
-_idx = 0
-idx = idxs[_idx]
+        self.display_frame()
 
-# start timer for persistent saving
-start_time = time.time()
 
-zoom = False
+        # TODO: here in general: make a dict of usable keys and print them
+        # cv2.putText(curr_img, name_indicators[0], (10, 900),
+        #             self.font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        # indicate current frame
 
-current_mask = [0, 0, 0, 0]
+        return
 
-print('starting GUI')
-while (True):
-  # fetch current roi
-  # FIXME: indexerror
-  try:
-    y1, x1, y2, x2 = results[idx]['rois'][0]
-  except IndexError:
-    idx = idx - 1
+    def check_keys(self):
 
-  if (idx == idxs[_idx]):
-    current_mask = [x1, y1, x2, y2]
-    # indicate mask
-    cv2.rectangle(fnames[idx],
-                  (current_mask[0], current_mask[1]),
-                  (current_mask[2], current_mask[3]),
-                  (0, 255, 0), 3)
-    # indicate mask number
-    cv2.putText(fnames[idx], 'Mask: ' + str(_idx), (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-  else:
-    cv2.rectangle(fnames[idx],
-                  (current_mask[0], current_mask[1]),
-                  (current_mask[2], current_mask[3]),
-                  (125, 125, 125), 3)
+        frameclick = cv2.waitKey(1) & 0xFF
+        # quit
+        if (frameclick == ord('q')):
+            self.break_status = True
+        if (frameclick == ord('a')):
+            while (True):
+                if (cv2.waitKey(20) & 0xFF == ord('a')):
+                    self.break_status = True
+                sleep(0.1)
+        # next frame
+        if (frameclick == ord('n')):
+            self.current_frame += self.stepsize
+            cv2.putText(self.frames[self.current_frame], 'Mask: ' + str(self.current_mask), (10, 100),
+                        self.font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        # previous frame
+        if (frameclick == ord('p')):
+            self.current_frame -= self.stepsize
+            cv2.putText(self.frames[self.current_frame], 'Mask: ' + str(self.current_mask), (10, 100),
+                        self.font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        # zoom in
+        if (frameclick == ord('=')):
+            self.zoom = True
 
-  ### things to show at every frame
-  
-  #TODO: here in general: make a dict of usable keys and print them
-  cv2.putText(fnames[idx],  name_indicators[0], (10, 900), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
-  # indicate current frame
-  cv2.putText(fnames[idx], 'Frame: ' + str(idx), (10, 200), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-  # plot image
-  if (zoom):
-    cv2.imshow("output", rescaled_img)  # Show image
-  else:
-    cv2.imshow("output", fnames[idx])  # Show image
-  frameclick = cv2.waitKey(1) & 0xFF
+        # zoom out
+        if (frameclick == ord('-')):
+            self.zoom = False
 
-  if (frameclick == ord('q')):
-    break
-  if (frameclick == ord('a')):
+        # change mask focus increasing
+        if (frameclick == ord('.')):
+            self.current_mask_focus += 1
+
+        # change mask focus decreasing
+        if (frameclick == ord(',')):
+            self.current_mask_focus -= 1
+
+        # TODO: implement
+        # manual mode trigger
+        # if (frameclick == ord('m')):
+        #     self.
+
+        for j in range(1, 5):
+            if (frameclick == ord(str(j))):
+                # TODO: multiple results are in same FOV
+                self.results[self.current_frame] = {
+                    'frame' : self.frames[self.current_frame],
+                    'masks' : self.masks[self.current_frame],
+                    'current_mask' : self.current_mask_focus,
+                    'identity' : self.name_indicators[j-1],
+                }
+                self.current_mask += 1
+                self.current_frame = self.random_mask_indexes[self.current_mask]
+                # TODO: implement this, or rather sample new random spots instead of prefixed list
+                # try:
+                #     self.current_frame = idxs[_idx]
+                # except IndexError:
+                #     continue
+        # skip a mask
+        # # FIXME: index errors at end
+        # if (frameclick == ord('d')):
+        #     self.results.append([self.frames[idx], self.masks[idx], 'd'])
+        #     _idx += 1
+        #     try:
+        #         idx = idxs[_idx]
+        #     except IndexError:
+        #         continue
+        # pass
+
+    def check_num_results(self):
+        if(len(self.results) == self.num_masks):
+            self.break_status = True
+            print('labeled enough masks')
+        return
+
+    def update(self):
+        self.display_current()
+        self.check_keys()
+        self.clocked_save()
+        self.check_num_results()
+        return self.break_status
+
+
+
+
+# FIXME: end when all masks
+    # # gone through all masks
+    # if (_idx == len(idxs)):
+    #     break
+
+
+
+def main():
+    # parse arguments
+    args = parser.parse_args()
+    names = args.names
+    names = names.split(',')
+    name_indicators = ['Key/Name:  ' + str(idx) + ' / ' + el for idx, el in enumerate(names)]
+
+    filename = args.filename
+    sink = './example_data/frames/' + filename + '_segmented/'
+    results_sink = args.results_sink
+    num_masks = args.num_masks
+    window_size = args.window_size
+
+    print('loading masks')
+    masks = np.load('./example_data/masks/' + filename + '_SegResults.npy', allow_pickle=True)
+
+    # create results folder if non-existing
+    if not os.path.exists(results_sink):
+        os.makedirs(results_sink)
+
+    # limit now to 1000 frames for demo purposes and limited masks
+    masks = masks[2000:2500]
+
+    # load a couple of frames
+    frames = []
+    print('loading frames')
+    for i in tqdm(range(2000,2500)):
+        example_frame = np.load(sink + filename + '_frame_' + str(i) + '.npy')
+        frames.append(example_frame)
+
+    stepsize = 1
+
+    # init handler
+    myhandler = WindowHandler(frames,
+                              name_indicators,
+                              filename,
+                              results_sink,
+                              masks,
+                              num_masks,
+                              stepsize,
+                              window_size)
+
     while (True):
-      if (cv2.waitKey(20) & 0xFF == ord('a')):
-        break
-      sleep(0.1)
-  if (frameclick == ord('n')):
-    idx += stepsize
-    cv2.putText(fnames[idx], 'Mask: ' + str(_idx), (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-  if (frameclick == ord('p')):
-    idx -= stepsize
-    cv2.putText(fnames[idx], 'Mask: ' + str(_idx), (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        status = myhandler.update()
+        if(status):
+            myhandler.save_data()
+            break
+        sleep(0.05)
 
-  # zoom in
-  if (frameclick == ord('=')):
-    from skimage.transform import rescale
 
-    img = fnames[idx]
-    center_x = float(x2 + x1) / 2.0
-    center_y = float(y2 + y1) / 2.0
-
-    # TODO: make relative
-    masked_img = img[int(center_y - 200):int(center_y + 200),
-                 int(center_x - 200):int(center_x + 200)]
-
-    # TODO: determine value or find best fixed
-    rescaled_img = rescale(masked_img, 2.5, multichannel=True)
-    zoom = True
-
-  # zoom out
-  if (frameclick == ord('-')):
-    zoom = False
-
-  for j in range(1, 5):
-    if (frameclick == ord(str(j))):
-      res.append([fnames[idx], results[idx], str(j)])
-      _idx += 1
-      try:
-        idx = idxs[_idx]
-      except IndexError:
-        continue
-
-  # skip a mask
-  # FIXME: index errors at end
-  if (frameclick == ord('d')):
-    res.append([fnames[idx], results[idx], 'd'])
-    _idx += 1
-    try:
-      idx = idxs[_idx]
-    except IndexError:
-      continue
-
-  # gone through all masks
-  if (_idx == len(idxs)):
-    break
-  # writing data in regular intervals
-  if ((time.time() - start_time) % 300 < 0.055):
-    print('writing data, do not interrupt!')
-    np.save(results_sink + 'IDresults_' + filename + '.npy', res)
-    print('done writing data')
-  sleep(0.05)
-
-cv2.destroyAllWindows()
-print('writing data, do not interrupt!')
-np.save(results_sink + 'IDresults_' + filename + '.npy', res)
-print('done writing data')
+if __name__ == '__main__':
+    main()
